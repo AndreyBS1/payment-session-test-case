b683aa655be23f70cc3856759a041d7acc435944
AndreyBS1 <andrey.babenkov.00@gmail.com>
2024-09-26 19:49:11 +0300

    feat: check if session started before start

--- src/api/start-session.ts
+++ src/api/start-session.ts
@@ -1,8 +1,13 @@
 export const startSession = async () => {
   const isSessionCreated = localStorage.getItem('is-session-created')
+  const startedAt = localStorage.getItem('session-started-at')
 
   if (isSessionCreated !== 'true') {
     throw new Error('Session does not exist')
+  }
+
+  if (startedAt) {
+    throw new Error('Session already started')
   }
 
   localStorage.setItem('session-started-at', Date.now().toString())
